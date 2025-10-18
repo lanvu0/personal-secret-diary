0a2d4709adabccd2be0e7dc98ed1775f9d40e2df
Lan <103476770+lanvu0@users.noreply.github.com>
2025-10-18 13:17:45 +1100

    Default to process.env.PORT for PORT number

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import entryRouter from './src/routes/entryRoutes.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 // Serve static files (CSS, JS) from the 'public' directory
